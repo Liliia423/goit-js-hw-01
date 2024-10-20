5a165758e3fd4cfc61e03e77831e37146d9afe8f
Liliia423 <etude4408@gmail.com>
2024-10-20 12:57:30 +0300

    tyu

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,14 @@
 'use strict';
-/*function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
-}
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));*/
-
 function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+/*function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
   const value = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   return value;
@@ -17,4 +18,4 @@ console.log(value);
 value = makeTransaction(3, 1000);
 console.log(value);
 value = makeTransaction(10, 500);
-console.log(value);
+console.log(value);*/
